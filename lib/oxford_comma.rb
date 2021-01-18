ff4f07e363805418a6a012ac3867ec5568551763
Joshua Cho <joshdavecho@protonmail.com>
2021-01-18 00:02:32 -0500

    commit tree ba5b169bb4c17cc090d832d15144c3c921f65f0c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        return "#{array[0]}"
+    elsif array.size == 2
+        return array.join(" and ")
+    elsif array.size >= 3
+        array_three_or_more = array
+        array_three_or_more[-1] = "and #{array[-1]}"
+        return array_three_or_more.join(", ")
+end
 end
